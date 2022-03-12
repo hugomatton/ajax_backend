fa712bb03ad58c5dd6dbd1813d78eeaf06d65aeb
hugomatton <hugomattonisis@gmail.com>
2022-03-12 21:06:53 +0100

    finito

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,4 +7,5 @@ INSERT INTO Country(code, name) VALUES
 INSERT INTO CITY(name, population, country_id) VALUES
     ('Paris', 12, SELECT id FROM Country WHERE code = 'FR'),
     ('London', 18, SELECT id FROM Country WHERE code = 'UK'),
-    ('New York', 27, SELECT id FROM Country WHERE code = 'US');
+    ('New York', 27, SELECT id FROM Country WHERE code = 'US'),
+    ('Chicago', 27, SELECT id FROM Country WHERE code = 'US');
